37facf87d0ae051fc70790ba70c739f087b4389e
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-06-19 15:29:44 -0400

    Hit MVP.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const server = require("./api/server.js");
+const express = require("express");
+const accountRouter = require("./routers/accountRouter");
+
+const server = express();
+server.use(express.json());
+server.use("/accounts", accountRouter);
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
